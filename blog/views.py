4d2706c45e70edf7b4abdc9d6417de0bb17ec5cf
khosro <khsro@gmail.com>
2024-07-15 16:33:54 +0330

    Fixed bug show single status true and optimize timezone

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, get_object_or_404
-from datetime import datetime
+from django.utils import timezone
 from .models import Post
 from .utils import increase_post_visits
 
 # Create your views here.
 
 def blog_view(request):
-    posts = Post.objects.filter(status=True, published_date__lt=datetime.utcnow())
+    posts = Post.objects.filter(status=True, published_date__lt=timezone.now())
     context = {"posts": posts}
     return render(request, "blog-home.html", context)
 
 def blog_single(request, pid):
-    post = get_object_or_404(Post, pk=pid)
+    posts = Post.objects.filter(status=True, published_date__lt=timezone.now())
+    post = get_object_or_404(posts, pk=pid)
     increase_post_visits(post)
     context = {"post": post}
     return render(request, "blog-single.html", context)
