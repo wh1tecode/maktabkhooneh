ce1a8d79257fccc72ddea60df53f997bb71ed8e8
khosro <khsro@gmail.com>
2024-07-17 19:40:17 +0330

    Fixed bug tags and published date

--- website/templatetags/website_tags.py
+++ website/templatetags/website_tags.py
@@ -1,11 +1,11 @@
 from django import template
 from blog.models import Post
-
+from django.utils import timezone
 
 register = template.Library()
 
 
 @register.inclusion_tag("website-latest-post.html")
 def latest_posts(arg=6):
-    posts = Post.objects.filter(status=True).order_by("-id")[0:arg]
+    posts = Post.objects.filter(status=True, published_date__lt=timezone.now()).order_by("-id")[0:arg]
     return {"posts": posts}
