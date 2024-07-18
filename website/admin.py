9cb56fd9568ca0ce68b32c31fd69717b9899c3ce
khosro <khsro@gmail.com>
2024-07-18 19:59:18 +0330

    Total update

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from website.models import Contact
 
 # Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ["name", "email", "subject", "created_date"]
--- website/views.py
+++ website/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
 from os.path import join
-
+from django.http import HttpRequest, HttpResponseRedirect
+from django.contrib import messages
+from .forms import ContactForm
 # Create your views here.
 
 
-def home_view(request):
+def home_view(request: HttpRequest):
     return render(request, "index.html")
 
 
-def about_view(request):
+def about_view(request: HttpRequest):
     return render(request, "about.html")
 
 
-def contact_view(request):
+def contact_view(request: HttpRequest):
+    if request.POST:
+        form = ContactForm(data=request.POST)
+        if form.is_valid():
+            new_form = form.save(commit=False)
+            new_form.name = "unknown"
+            new_form.save()
+            messages.add_message(request=request, level=messages.SUCCESS, message="Message sent successfully")
+            return HttpResponseRedirect("/contact")
+        else:
+            messages.add_message(request=request, level=messages.ERROR, message="Sending message failed")
+            return HttpResponseRedirect("/")
     return render(request, "contact.html")
