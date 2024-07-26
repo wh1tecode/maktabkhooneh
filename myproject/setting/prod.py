a1240b26528b2128464c8b313c1a638b648658a4
khosro <khsro@gmail.com>
2024-07-26 20:14:36 +0330

    Update dev and productions

---
+++ myproject/setting/prod.py
@@ -0,0 +1,49 @@
+from myproject.settings import *
+from pathlib import Path
+from os.path import join
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-xj$o!zg@)vs2k1#c80y%%(e=1b^%i2d5rw3vv$m$jmcl#@67&f"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+
+MEDIA_URL = "/media/"
+
+STATIC_ROOT = join(BASE_DIR, "static")
+
+MEDIA_ROOT = join(BASE_DIR, "media")
+
+
+# site
+SITE_ID = 2
+
+# robots
+ROBOTS_USE_HOST = False
+ROBOTS_USE_SITEMAP = False
+
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+
+
+# smtp 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_FROM = "kickfundingapp@gmail.com"
+EMAIL_HOST_USER = "jojojumung@gmail.com"
+EMAIL_HOST_PASSWORD = "qiuk ghdd fbge infe"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
--- myproject/settings.py
+++ myproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
 from pathlib import Path
 from os.path import join
 
@@ -17,16 +18,8 @@ from os.path import join
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xj$o!zg@)vs2k1#c80y%%(e=1b^%i2d5rw3vv$m$jmcl#@67&f"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -52,12 +45,6 @@ INSTALLED_APPS = [
     "accounts",
 ]
 
-# site
-SITE_ID = 2
-
-# robots
-ROBOTS_USE_HOST = False
-ROBOTS_USE_SITEMAP = False
 
 # multi captcha
 MULTI_CAPTCHA_ADMIN = {
@@ -149,11 +136,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-MEDIA_URL = "/media/"
-
-STATIC_ROOT = join(BASE_DIR, "static")
-
-MEDIA_ROOT = join(BASE_DIR, "media")
 
 STATICFILES_DIRS = [
     join(BASE_DIR, "statics"),
@@ -165,18 +147,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+
 
 LOGIN_URL = "accounts:login"
-
-# smtp 
-
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_FROM = "kickfundingapp@gmail.com"
-EMAIL_HOST_USER = "jojojumung@gmail.com"
-EMAIL_HOST_PASSWORD = "qiuk ghdd fbge infe"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
