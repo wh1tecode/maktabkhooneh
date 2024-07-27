2c5abeefdd03fa533d6a3126a1cd4c37a4951b76
khosro <khosro@local.com>
2024-07-27 13:54:30 +0330

    update

--- myproject/setting/dev.py
+++ myproject/setting/dev.py
@@ -3,12 +3,6 @@ from myproject.settings import *
 from pathlib import Path
 from os.path import join
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-xj$o!zg@)vs2k1#c80y%%(e=1b^%i2d5rw3vv$m$jmcl#@67&f"
